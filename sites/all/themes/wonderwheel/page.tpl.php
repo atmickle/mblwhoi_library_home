6e609bc6d33ba3c771462b0319046984e6199496
adorsk-whoi <adorsk@whoi.edu>
2011-03-24 14:26:44 -0400

    added tim's code as a clone, made symlinks

---
+++ sites/all/themes/wonderwheel/page.tpl.php
@@ -0,0 +1,189 @@
+<?php
+// $Id: page.tpl.php,v 1.13 2008/09/15 08:31:58 johnalbin Exp $
+
+/**
+ * @file page.tpl.php
+ *
+ * Theme implementation to display a single Drupal page.
+ *
+ * Available variables:
+ *
+ * General utility variables:
+ * - $base_path: The base URL path of the Drupal installation. At the very
+ *   least, this will always default to /.
+ * - $css: An array of CSS files for the current page.
+ * - $directory: The directory the theme is located in, e.g. themes/garland or
+ *   themes/garland/minelli.
+ * - $is_front: TRUE if the current page is the front page. Used to toggle the mission statement.
+ * - $logged_in: TRUE if the user is registered and signed in.
+ * - $is_admin: TRUE if the user has permission to access administration pages.
+ *
+ * Page metadata:
+ * - $language: (object) The language the site is being displayed in.
+ *   $language->language contains its textual representation.
+ *   $language->dir contains the language direction. It will either be 'ltr' or 'rtl'.
+ * - $head_title: A modified version of the page title, for use in the TITLE tag.
+ * - $head: Markup for the HEAD section (including meta tags, keyword tags, and
+ *   so on).
+ * - $styles: Style tags necessary to import all CSS files for the page.
+ * - $scripts: Script tags necessary to load the JavaScript files and settings
+ *   for the page.
+ * - $body_classes: A set of CSS classes for the BODY tag. This contains flags
+ *   indicating the current layout (multiple columns, single column), the current
+ *   path, whether the user is logged in, and so on.
+ *
+ * Site identity:
+ * - $front_page: The URL of the front page. Use this instead of $base_path,
+ *   when linking to the front page. This includes the language domain or prefix.
+ * - $logo: The path to the logo image, as defined in theme configuration.
+ * - $site_name: The name of the site, empty when display has been disabled
+ *   in theme settings.
+ * - $site_slogan: The slogan of the site, empty when display has been disabled
+ *   in theme settings.
+ * - $mission: The text of the site mission, empty when display has been disabled
+ *   in theme settings.
+ *
+ * Navigation:
+ * - $search_box: HTML to display the search box, empty if search has been disabled.
+ * - $primary_links (array): An array containing primary navigation links for the
+ *   site, if they have been configured.
+ * - $secondary_links (array): An array containing secondary navigation links for
+ *   the site, if they have been configured.
+ *
+ * Page content (in order of occurrance in the default page.tpl.php):
+ * - $left: The HTML for the left sidebar.
+ *
+ * - $breadcrumb: The breadcrumb trail for the current page.
+ * - $title: The page title, for use in the actual HTML content.
+ * - $help: Dynamic help text, mostly for admin pages.
+ * - $messages: HTML for status and error messages. Should be displayed prominently.
+ * - $tabs: Tabs linking to any sub-pages beneath the current page (e.g., the view
+ *   and edit tabs when displaying a node).
+ *
+ * - $content: The main content of the current Drupal page.
+ *
+ * - $right: The HTML for the right sidebar.
+ *
+ * Footer/closing data:
+ * - $feed_icons: A string of all feed icons for the current page.
+ * - $footer_message: The footer message as defined in the admin settings.
+ * - $footer : The footer region.
+ * - $closure: Final closing markup from any modules that have altered the page.
+ *   This variable should always be output last, after all other dynamic content.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_page()
+ */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
+
+<head>
+  <title><?php print $head_title; ?></title>
+  <?php print $head; ?>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <script type="text/javascript"><?php /* Needed to avoid Flash of Unstyled Content in IE */ ?> </script>
+</head>
+<body class="<?php print $body_classes; ?>">
+	
+<div id="bodwrap">
+	    <div id="header">
+	
+			<div id="topstrip">
+				<div class="page">
+					<?php print $topstrip; ?>
+				</div> <!-- end page -->
+			</div> <!-- end topstrip -->
+	    
+	   		
+		    <div class="page">
+		          <h1 id="site-name"><a href="/"><?php print $site_name; ?></a></h1>
+		          
+		      <?php if ($header): ?>
+		          <?php print $header; ?>
+		      <?php endif; ?>
+		
+		    </div><!-- end .page -->
+		    
+		    
+		    
+	    </div><!-- end #header -->
+		
+		   <div id="nav">
+		    	<div class="page">
+		    		<?php print $nav; ?>
+		    	</div>
+		    </div>
+		    
+		    <div id="search" class="strip">
+		    	<div class="page">
+		    		<h3>Search</h3>
+		    		<div id="searchtabs" class="hp-section">
+		    			
+		    			<?php print $search; ?>
+		    		</div> <!-- end searchtabs -->
+		    	</div>
+		    
+		    </div>
+		    
+		  
+	    <div id="main" class="strip">
+			
+		  
+	      <div id="content" class="page">
+				<?php if ($content_top): ?>
+		          <div id="content-top">
+		            <?php print $content_top; ?>
+		          </div> <!-- /#content-top -->
+		        <?php endif; ?>
+		
+		        <?php if ($breadcrumb or $tabs or $help or $messages): ?>
+		          <div id="content-header">
+		           
+		           <div id="crumbs"><?php print $breadcrumb; ?></div>
+		           
+		            <?php print $messages; ?>
+		            <?php if ($tabs): ?>
+		              <div class="tabs"><?php print $tabs; ?></div>
+		            <?php endif; ?>
+		            <?php print $help; ?>
+		          </div> <!-- /#content-header -->
+		        <?php endif; ?>
+		
+		        <div id="content-area">
+		        <div <?php if ($sidebar): ?>class="with-sidebar"<?php endif; ?>/>	
+		          <h2><?php print $title; ?></h2>
+		          <?php print $content; ?>
+		        </div>
+		          
+		          
+		          <?php if ($sidebar): ?>
+		              <div id="sidebar"><?php print $sidebar; ?></div>
+		            <?php endif; ?>
+		        </div>
+		
+				
+		
+		        <?php if ($content_bottom): ?>
+		          <div id="content-bottom">
+		            <?php print $content_bottom; ?>
+		          </div> <!-- /#content-bottom -->
+		        <?php endif; ?>
+	
+	      </div> <!-- end #content -->      
+	      
+	
+	    </div> <!-- end #main -->
+
+  			<div class="push"></div>
+  	  </div> <!-- end #bodwrap -->
+	  <div id="footer">
+		<div class="page">
+	    <?php print $footer; ?>
+		</div> <!-- end .page -->
+	  </div> <!-- end #footer -->
+	  
+	<?php print $closure; ?>
+</body>
+</html>
