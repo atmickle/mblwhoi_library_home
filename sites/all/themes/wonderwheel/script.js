f98b648bbfca2f6b154804a2895ce5f4813265aa
adorsk-whoi <adorsk@whoi.edu>
2011-03-25 08:46:31 -0400

    took out submodules references, add code from tim

---
+++ sites/all/themes/wonderwheel/script.js
@@ -0,0 +1,60 @@
+
+	$(document).ready(function(){
+
+		// Disable filefield icons.
+		$('.filefield-file .filefield-icon').hide(); 
+		$('.filefield-file div div').hide();
+	
+		// Add pdf icons to pdf links
+		$("a[href$='.pdf']").addClass("iconize pdf");
+ 
+		// Add txt icons to document links (doc, rtf, txt)
+		$("a[href$='.doc'], a[href$='.txt'], a[href$='.rft']").addClass("iconize txt");
+ 
+		// Add email icons to email links
+		$("a[href^='mailto:']").addClass("iconize email");
+ 
+		
+		$(function() {
+		    $(".view-author-pubs2 .item-list").jCarouselLite({
+		        btnNext: "#auth-btns .next",
+		        btnPrev: "#auth-btns .prev",
+		        scroll: 3
+
+		    });
+		});
+		
+		$(function() {
+		    $(".view-species-pubs .item-list").jCarouselLite({
+		        btnNext: "#spec-btns .next",
+		        btnPrev: "#spec-btns .prev",
+		        scroll: 3
+
+		    });
+		});
+		
+		$('.view-species-pubs').addClass('hidepub');
+		
+		$('#auth-tab').click(function () {
+	      $('.view-author-pubs2').removeClass('hidepub');
+	      $('.view-species-pubs').addClass('hidepub');
+	      $('#auth-tab').addClass('active-pubs');
+	      $('#spec-tab').removeClass('active-pubs');
+	      
+	    });
+	    
+	     $('#spec-tab').click(function () {
+	      $('.view-author-pubs2').addClass('hidepub');
+	      $('.view-species-pubs').removeClass('hidepub');
+	      $('#auth-tab').removeClass('active-pubs');
+	      $('#spec-tab').addClass('active-pubs');
+	    });
+		
+				
+		$('#block-custom_search_blocks-1 .content').append('<div class="adv-link"><a href="/research-databases/a">Browse All</a></div>');
+
+		
+
+		
+	});	
+
