6ed3d84e4a7cab62c846390c981f57a3b7b959fa
adorsk-whoi <adorsk@whoi.edu>
2011-09-20 08:26:14 -0700

    added tab remembrance code

--- sites/all/themes/wonderwheel/script.js
+++ sites/all/themes/wonderwheel/script.js
@@ -1,3 +1,6 @@
+/* jQuery Cookie */
+jQuery.cookie=function(key,value,options){if(arguments.length>1&&String(value)!=="[object Object]"){options=jQuery.extend({},options);if(value===null||value===undefined){options.expires=-1}if(typeof options.expires==='number'){var days=options.expires,t=options.expires=new Date();t.setDate(t.getDate()+days)}value=String(value);return(document.cookie=[encodeURIComponent(key),'=',options.raw?value:encodeURIComponent(value),options.expires?'; expires='+options.expires.toUTCString():'',options.path?'; path='+options.path:'',options.domain?'; domain='+options.domain:'',options.secure?'; secure':''].join(''))}options=value||{};var result,decode=options.raw?function(s){return s}:decodeURIComponent;return(result=new RegExp('(?:^|; )'+encodeURIComponent(key)+'=([^;]*)').exec(document.cookie))?decode(result[1]):null};
+
 $(document).ready(function(){
 
 		    // Disable filefield icons.
@@ -62,6 +65,61 @@ $(document).ready(function(){
 									visible: 2,
 									scroll: 2
 								      });
+
+		    $('#SearchJournalTitle').submit(function() {
+		    	$.cookie('SearchJournal_search',$('#param_pattern_value').val());
+		    	$.cookie('SearchJournal_type',$('input[name=param_textSearchType_value]:checked').attr('id'));
+		    	$.cookie('last_form','SearchJournal', { path: '/' });
+		    	});
+		    $('#search-block-form').submit(function() {
+		    	$.cookie('last_form','SearchSite', { path: '/' });
+		    	$.cookie('SearchSite_search',$('#edit-search-block-form-1').val());
+		    });
+		    $('#searchBasic').submit(function() {
+		    	$.cookie('last_form','SearchLibrary', { path: '/' });
+		    	$.cookie('SearchLib_search',$('#searchArg').val());
+		    });
+		    $('.alpha-link').click(function() {
+		    	$.cookie('last_form','SearchDatabase', { path: '/' });
+		    });
+		    var SearchSite_search = $.cookie('SearchSite_search');
+		    if (SearchSite_search!=''){
+		    	$('#edit-search-block-form-1').val(SearchSite_search);
+		    }
+		    var SearchLib_search = $.cookie('SearchLib_search');
+		    if (SearchLib_search!=''){
+		    	$('#searchArg').val(SearchLib_search);
+		    }
+		    var SearchJournal_search = $.cookie('SearchJournal_search');
+		    if (SearchJournal_search!=''){
+		    	$('#param_pattern_value').val(SearchJournal_search);
+		    }
+		    var SearchJournal_type = $.cookie('SearchJournal_type');
+		    if (SearchJournal_type=='contains'){
+		    	$("#contains").attr('checked', true);
+		    }else{
+		    	$("#startsWith").attr('checked', true);
+		    }
+		    var last_form = $.cookie('last_form');
+		    switch (last_form){
+		    case 'SearchJournal': $('#quicktabs-tab-1-0').click();break;
+		    case 'SearchDatabase': $('#quicktabs-tab-1-1').click();break;
+		    case 'SearchLibrary': $('#quicktabs-tab-1-2').click();break;
+		    case 'SearchSite': $('#quicktabs-tab-1-3').click();break;
+		    }
+		    $('#quicktabs-tab-1-0').click(function() {
+		    	$.cookie('last_form','SearchJournal', { path: '/' });
+		    	});
+		    $('#quicktabs-tab-1-1').click(function() {
+		    	$.cookie('last_form','SearchDatabase', { path: '/' });
+		    });
+		    $('#quicktabs-tab-1-2').click(function() {
+		    	$.cookie('last_form','SearchLibrary', { path: '/' });
+		    });
+		    $('#quicktabs-tab-1-3').click(function() {
+		    	$.cookie('last_form','SearchSite', { path: '/' });
+		    });
+
 
 		  });
 
